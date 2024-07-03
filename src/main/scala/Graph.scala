74d49b1fe9439ec4689b302bef9075ba594773fe
Zyad Zekri <72571140+BadTimeGiver@users.noreply.github.com>
2024-07-03 10:30:33 +0200

    Implemented addVertex

--- src/main/scala/Graph.scala
+++ src/main/scala/Graph.scala
@@ -1,5 +1,14 @@
+import scala.collection.mutable
 case class Edge(to: Int, weight: Int)
 case class Node(id: Int, edges: List[Edge])
 case class GraphInformations(name: String, isWeighted: Boolean, isBidirectional: Boolean)
-case class Graph(graphInformations: GraphInformations, nodes: List[Node])
+case class Graph(graphInformations: GraphInformations, nodes: List[Node]) {
+    private val nodeMap: mutable.Map[Int, Node] = mutable.Map(nodes.map(node => node.id -> node): _*)
+    def addVertex(id: Int): Graph = {
+        if (!nodeMap.contains(id)) {
+            nodeMap += (id -> Node(id, List()))
+        }
+        copy(nodes = nodeMap.values.toList)
+    }
+}
 
