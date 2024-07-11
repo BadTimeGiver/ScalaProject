e5dcb171e37ba2724479460084d607a739cb3eba
Zyad Zekri <72571140+BadTimeGiver@users.noreply.github.com>
2024-07-11 21:31:15 +0200

    GraphSerizalization implemented

---
+++ core/src/main/scala/GraphSerialization.scala
@@ -0,0 +1,23 @@
+import zio.json._
+
+object GraphSerialization {
+    implicit val edgeDecoder: JsonDecoder[Edge] = DeriveJsonDecoder.gen[Edge]
+    implicit val edgeEncoder: JsonEncoder[Edge] = DeriveJsonEncoder.gen[Edge]
+
+    implicit val nodeDecoder: JsonDecoder[Node] = DeriveJsonDecoder.gen[Node]
+    implicit val nodeEncoder: JsonEncoder[Node] = DeriveJsonEncoder.gen[Node]
+
+    implicit val graphInformationsDecoder: JsonDecoder[GraphInformations] = DeriveJsonDecoder.gen[GraphInformations]
+    implicit val graphInformationsEncoder: JsonEncoder[GraphInformations] = DeriveJsonEncoder.gen[GraphInformations]
+
+    implicit val graphDecoder: JsonDecoder[Graph] = DeriveJsonDecoder.gen[Graph]
+    implicit val graphEncoder: JsonEncoder[Graph] = DeriveJsonEncoder.gen[Graph]
+
+    def toJSON(graph: Graph): String ={
+        graph.toJsonPretty
+    }
+
+    def fromJSON(json: String) : Either[Graph,String]={
+        json.fromJson[Graph]
+    }
+}
